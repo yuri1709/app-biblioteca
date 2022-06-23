1cb6f87c4b9474ad39c6f053550609096984854a
ozil <36127512021.2@senacrjedu.local>
2022-06-23 11:51:00 -0300

    criada a classe teca

---
+++ teca/src/teca/Teca.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teca;
+
+/**
+ *
+ * @author 36127512021.2
+ */
+public class Teca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame telaMenu = new JFrame();
+        JFrame telaLogin = new JFrame();
+                       
+    }
+    
+}
