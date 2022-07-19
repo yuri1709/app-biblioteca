2c37adeb111c3052936cdcc5c5020d82d2aa3c13
yuri1709 <yuriconder184@gmail.com>
2022-07-18 23:29:32 -0300

    Add inserir

--- teca/src/teca/controller/EmprestimoLivroDAO.java
+++ teca/src/teca/controller/EmprestimoLivroDAO.java
@@ -3,8 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package teca.controller;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
 import teca.model.EmprestimoLivro;
 
 public class EmprestimoLivroDAO {
+       String url = "jdbc:mysql://localhost/tecadb";
+       String sql;
        
+       public void inserir(EmprestimoLivro EML) {
+           String sql = "INSERT INTO emp (emprestimo_id, matricula, registro) values ('"+EML.getEmprestimo_id()+"', '"+EML.getMatricula()+"','"+EML.getRegistro()+"')"; 
+        try
+          {
+               Connection conexao = DriverManager.getConnection(url,"root","");
+               PreparedStatement atualizar = conexao.prepareStatement(sql);
+               atualizar.executeUpdate();
+               JOptionPane.showMessageDialog(null,"Emprestimo realizado!");
+          }
+
+          catch(Exception erro){ 
+              JOptionPane.showMessageDialog(null,"Erro na Conexão com Banco de Dados : "+erro);
+          }      
+       }
 }
